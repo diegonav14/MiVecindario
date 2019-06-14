666e7e89e40c41056af53867a8958c50ea4db3cc
DiegoNavarro14 <32942483+DiegoNavarro14@users.noreply.github.com>
2019-06-14 02:37:33 -0400

    V.0.0.6

--- app/src/main/java/com/example/mivecindario/IniciarSesion.java
+++ app/src/main/java/com/example/mivecindario/IniciarSesion.java
@@ -1,6 +1,8 @@
 package com.example.mivecindario;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -26,7 +28,6 @@ import com.google.firebase.database.ValueEventListener;
 public class IniciarSesion extends AppCompatActivity  {
 
 
-
     EditText editTextCorreo, editTextPass;
 
     FirebaseDatabase firebaseDatabase;
@@ -41,21 +42,33 @@ public class IniciarSesion extends AppCompatActivity  {
         editTextCorreo = findViewById(R.id.txt_correo);
         editTextPass = findViewById(R.id.txt_pass);
 
+
         //Toolbar toolbar = findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
+        cargarPreferencias();
         incializarFirebase();
 
         }
 
+        private void cargarPreferencias() {
 
-
+            SharedPreferences preferencias = getSharedPreferences("sesion", Context.MODE_PRIVATE);
+            if (preferencias.contains("nombreAdmin") && preferencias.contains("apellidoAdmin")){
+                Intent intent = new Intent(this,Inicio.class);
+                startActivity(intent);
+            }else if(preferencias.contains("nombreUsuario") && preferencias.contains("apellidoUsuario")){
+                Intent intent = new Intent(this,InicioUsuario.class);
+                startActivity(intent);
+            }
+        }
 
         private void incializarFirebase() {
             FirebaseApp.initializeApp(this);
             firebaseDatabase = FirebaseDatabase.getInstance();
             databaseReference = firebaseDatabase.getReference();
         }
+
 
 
         public void IniciarSesion ( View v) {
@@ -72,8 +85,11 @@ public class IniciarSesion extends AppCompatActivity  {
 
                         if (correoAdmin.equals(admin.getCorreo()) && passAdmin.equals(admin.getPassword())){
                             Intent intentAdmin = new Intent(IniciarSesion.this, Inicio.class);
-                            intentAdmin.putExtra("nombreAdmin",admin.getNombre());
-                            intentAdmin.putExtra("apellidoAdmin",admin.getApellido());
+                            SharedPreferences preferencias = getSharedPreferences("sesion", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = preferencias.edit();
+                            editor.putString("nombreAdmin", admin.getNombre());
+                            editor.putString("apellidoAdmin",admin.getApellido());
+                            editor.commit();
                             startActivity(intentAdmin);
                         }
 
@@ -96,9 +112,12 @@ public class IniciarSesion extends AppCompatActivity  {
 
                         if (correo.equals(user.getCorreo()) && pass.equals(user.getPassword())){
                             Intent intent = new Intent (IniciarSesion.this, InicioUsuario.class);
-                            intent.putExtra("nombreUsuario",user.getNombre());
-                            intent.putExtra("apellidoUsuario",user.getApellido());
-                            intent.putExtra("tipoUsuario", user.getTipo());
+                            SharedPreferences preferencias = getSharedPreferences("sesion", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = preferencias.edit();
+                            editor.putString("nombreUsuario", user.getNombre());
+                            editor.putString("apellidoUsuario", user.getApellido());
+                            editor.putString("tipoUsuario", user.getTipo());
+                            editor.commit();
                             startActivity(intent);
                         }
                     }
--- app/src/main/java/com/example/mivecindario/InicioUsuario.java
+++ app/src/main/java/com/example/mivecindario/InicioUsuario.java
@@ -1,9 +1,36 @@
 package com.example.mivecindario;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.mivecindario.Modelos.Evento;
+import com.example.mivecindario.Modelos.Hogar;
+import com.example.mivecindario.Modelos.Usuario;
+import com.example.mivecindario.Modelos.Vecindario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class InicioUsuario extends AppCompatActivity {
 
@@ -11,53 +38,146 @@ public class InicioUsuario extends AppCompatActivity {
 
     String tipoUsuario,nmUsuario,apUsuario;
 
+    EditText comentarioEvento;
+
+    Button btnEvento;
+
+    ListView lv_Eventos;
+
+    private List<Evento> listaEventos = new ArrayList<Evento>();
+    ArrayAdapter<Evento> arrayAdapteEvento;
+
+    Spinner tipoEvento;
+
+    TextView tpUsuario;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_usuario);
 
+
+
+        tpUsuario= findViewById(R.id.txt_tipoUsuario);
+        comentarioEvento = findViewById(R.id.txt_comentarioEvento);
+        tipoEvento = findViewById(R.id.spEvento);
+        btnEvento = findViewById(R.id.btnEventos);
+        lv_Eventos = findViewById(R.id.lv_eventos);
+
+        ArrayAdapter<CharSequence> adapterSpinner = ArrayAdapter.createFromResource(this,R.array.TipoEvento, android.R.layout.simple_spinner_dropdown_item);
+
+        tipoEvento.setAdapter(adapterSpinner);
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        recibirDatos();
+        inicializarFirebase();
+        cargarPreferencias();
+        listarDatos();
         tiposUsuarios();
 
     }
 
-    private void recibirDatos() {
-        Bundle datoExtra = getIntent().getExtras();
-        nmUsuario = datoExtra.getString("nombreUsuario");
-        apUsuario = datoExtra.getString("apellidoUsuario");
-        tipoUsuario = datoExtra.getString("tipoUsuario");
-        toolbar.setSubtitle(nmUsuario+" "+apUsuario);
-        toolbar.setSubtitleTextColor(getResources().getColor(R.color.colorPrimaryDark));
-    }
+
 
     private void tiposUsuarios(){
 
         switch (tipoUsuario){
 
             case "Presidente junta vecinos":{
-
+                tpUsuario.setText(tipoUsuario);
+                btnEvento.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ingresarEvento();
+                    }
+                });
                 break;
             }
 
             case "Jefe de hogar":{
-
+                tpUsuario.setText(tipoUsuario);
+                btnEvento.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ingresarEvento();
+                    }
+                });
                 break;
             }
 
             case "Miembro de hogar":{
-
+                tpUsuario.setText(tipoUsuario);
+                btnEvento.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ingresarEvento();
+                    }
+                });
                 break;
+
+
             }
 
         }
     }
 
+    private void ingresarEvento() {
+        databaseReference.child("Usuario").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String comentario = comentarioEvento.getText().toString();
+                String tipo = tipoEvento.getSelectedItem().toString();
 
+                if (comentario.equals("") || tipo.equals("Tipo")){
+                    validacion();
+                }
+                else{
+                    Evento e = new Evento();
+                    e.setUid(UUID.randomUUID().toString());
+                    e.setComentario(comentario);
+                    e.setTipo(tipo);
+                    for (DataSnapshot objSnapshot : dataSnapshot.getChildren()){
+                        Usuario u = objSnapshot.getValue(Usuario.class);
+                        if (nmUsuario.equals(u.getNombre()) && apUsuario.equals(u.getApellido())){
+                            e.setUsuario(u);
+                        }
+                    }
+                    databaseReference.child("Evento").child(e.getUid()).setValue(e);
+                    Toast.makeText(InicioUsuario.this, "Evento agregado", Toast.LENGTH_LONG).show();
+                    limpiarCajas();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
+    }
 
+    private void listarDatos() {
+        databaseReference.child("Evento").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listaEventos.clear();
+                for (DataSnapshot objSnapshot : dataSnapshot.getChildren()){
+                    Evento e = objSnapshot.getValue(Evento.class);
+                    listaEventos.add(e);
+                    arrayAdapteEvento = new ArrayAdapter<Evento>(InicioUsuario.this, android.R.layout.simple_list_item_1, listaEventos);
+                    lv_Eventos.setAdapter(arrayAdapteEvento);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    } // Listar los los usuarios
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,33 +185,68 @@ public class InicioUsuario extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    //@Override
-    //public boolean onOptionsItemSelected(MenuItem item) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        //switch (item.getItemId()){
+        switch (item.getItemId()){
 
-            //case R.id.icon_add:{
+            case R.id.icon_add:{
 
-                //break;
-            //}
+                break;
+            }
 
-            //case R.id.icon_save:{
-                //break;
-            //}
+            case R.id.icon_save:{
+                break;
+            }
 
-            //case R.id.icon_delete:{
-                //break;
-            //}
+            case R.id.icon_delete:{
+                break;
+            }
 
-           // case R.id.icon_login:{
+           case R.id.icon_login:{
+               SharedPreferences preferecias = getSharedPreferences("sesion", Context.MODE_PRIVATE);
+               SharedPreferences.Editor editor = preferecias.edit();
+               editor.clear();
+               editor.apply();
+               Intent intent = new Intent(this,IniciarSesion.class);
+               startActivity(intent);
+            }
+            default:break;
 
-            //}
-            //default:break;
+        }
+        return true;
+    }
 
-        //}
-        //return true;
-    //}
+    private void validacion() {
 
+        String comentario = comentarioEvento.getText().toString();
 
+        if (comentario.equals("")) {
+            comentarioEvento.setError("Requerido");
+        } else if (tipoEvento.getSelectedItem().toString().equals("Tipo")) {
+            Toast.makeText(this,"Debe seleccionar el tipo", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void limpiarCajas() {
+
+        comentarioEvento.setText("");
+        tipoEvento.setSelection(0);
+
+    }
+
+    private void inicializarFirebase() {
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+    }
+
+    private void cargarPreferencias() {
+        SharedPreferences preferecias = getSharedPreferences("sesion", Context.MODE_PRIVATE);
+        nmUsuario = preferecias.getString("nombreUsuario","NoSesion");
+        apUsuario = preferecias.getString("apellidoUsuario","NoSesion");
+        tipoUsuario = preferecias.getString("tipoUsuario","NoSesion");
+        toolbar.setSubtitle(nmUsuario+" "+apUsuario);
+    }
 
 }
